4f87d2047680330eadcc9fff67a33b3343590a52
Atdhe Boshnjaku <atdhe@Atdhes-MacBook-Pro-2.local>
2022-02-15 16:55:14 +0100

    Updating composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0065b331c5a37c92b37112f8254e5d3520df3158',
+        'name' => 'atdhe/tvshowsapi',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'atdhe/tvshowsapi' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0065b331c5a37c92b37112f8254e5d3520df3158',
+            'dev_requirement' => false,
+        ),
+    ),
+);
